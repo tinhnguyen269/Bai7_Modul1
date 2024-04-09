7f09de37f53923df624ec5e28b84e3b6122d804e
tinhnguyen269 <tinhnguyen180507@gmail.com>
2024-04-09 17:04:42 +0700

    tinh

---
+++ Thực hành/dientich.js
@@ -0,0 +1,7 @@
+inputwidth = prompt("Enter the width:")
+inputhight = prompt("Enter the hight:")
+let wight = parseInt(inputwidth);
+let hight = parseInt(inputhight);
+let area = wight*hight;
+
+document.write("the area is :",area);
